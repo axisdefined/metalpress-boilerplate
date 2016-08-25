847a0fc18e31ef651ef2c54f1570f0b5ba6d38ac
Cameron J Roe <cameronjroe@gmail.com>
2016-08-25 15:09:12 -0400

    fix(boilerplate): update boilerplate

--- build.js
+++
@@ -1,6 +0,0 @@
-var metalpress = require('metalpress');
-var config = require('./metalpress.config');
-
-metalpress.default(config, function (err, files) {
-  console.log('Site build completed: ', new Date());
-});
